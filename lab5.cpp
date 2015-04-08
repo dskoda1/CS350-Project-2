9cd05e56473515d9cec268077fc06aca031a556d
dskoda1 <dskoda1@binghamton.edu>
2015-04-08 14:00:30 -0400

    Compiler support for c++11

--- lab5.cpp
+++ lab5.cpp
@@ -4,22 +4,19 @@ using namespace std;
 #include <iostream>
 #include <algorithm>
 #include <map>
+#include <array>
 
 int main(int argc, char ** argv)
 {
 
-/**	Check for appropriate arguments
+/*	Check for appropriate arguments
 **	Must be in the form ./program2 <frames-of-memory> <input-file>
-**/
+*/
 	if(argc != 3)
 	{
 		cerr << "Invalid arguments passed: Must be in the form" << endl;
 		cerr << "   ./program2 <frames-of-memory> <input-file>" << endl;
 	}
-
-
-
-
 
 
 	return 0;
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
-CXX=g++
-FLAGS= -g -Wall -std=c++11
+CXX = g++-4.9
+FLAGS = -g -Wall -std=c++11
 EXEC = lab5
 TARGET = lab5
 
 all: $(TARGET).o
 	$(CXX) $(FLAGS) -o $(EXEC) $(TARGET).o
 
-$(TARGET): $(TARGET)
+$(TARGET).o: $(TARGET).cpp
 	$(CXX) $(FLAGS) -c $(TARGET).cpp
 
 clean:
