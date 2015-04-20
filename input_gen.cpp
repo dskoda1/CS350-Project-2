de3cdc9b0e47203e331c7dc72083f9aea28b1539
ncortina <ncortin1@binghamton.edu>
2015-04-20 14:34:43 -0400

    Updated Test File Generator
    Added in option to have total random references or have locality of reference with accesses.

--- input_gen.cpp
+++ input_gen.cpp
@@ -16,7 +16,8 @@ int main(int argc, char ** argv){
 	int numProc;//number of processes, change later
 	int addrSize;//address space size, change later
 	int numRef;//number of references, change later
-
+	int pattern;//pattern used for locality of reference, 0 for locality of ref, 1 for random	
+	
 	if(argc != 5)
 	{
 		cerr << "Usage: ./input_gen <num_proc> <proc_addr_size> <#_ref> <pattern> " << endl;
@@ -26,6 +27,7 @@ int main(int argc, char ** argv){
 	numProc = atoi(argv[1]);
 	addrSize = atoi(argv[2]);
 	numRef = atoi(argv[3]);
+	pattern = atoi(argv[4]);
 	//TODO: parse pattern string
 
 
@@ -36,27 +38,71 @@ int main(int argc, char ** argv){
 
 	std::ofstream testfile;
 	testfile.open("input.txt");
-	//write START lines to file
-	for(int i = 1; i < numProc + 1; i++)
-	{
-		testfile << "START " << i << " " << addrSize << "\n";
-
-	}
-	//write REFERENCE lines to file
-	for(int i = 0; i < numRef; i++){
-		
-		x = rand() % numProc + 1;
-		y = rand() % addrSize +  1;
-		testfile << "REFERENCE " << x << " " << y << "\n";
-
-	}
-	//write TERMINATE lines to file
-	for(int i = 1; i < numProc + 1; i++){
-		
-		testfile << "TERMINATE " << i << "\n";
-
-	}
 	
+	if(pattern == 0)//locality of reference case
+	{
+		int refPerProc = numRef / numProc;
+		int extraRef = 0;
+		while(numRef % refPerProc != 0)
+		{
+			refPerProc--;
+			extraRef++;
+		}
+		
+		//write START lines to file
+		for(int i = 1; i < numProc + 1; i++)
+		{
+			testfile << "START " << i << " " << addrSize << "\n";
+		}
+
+		//write REFERENCE lines to file
+		for(int i = 0; i < numProc; i++)
+		{	
+			for(int j = 0; j < refPerProc; j++)
+			{
+				y = rand() % addrSize +  1;
+				testfile << "REFERENCE " << i+1 << " " << y << "\n";
+				/*x = rand() % numProc + 1;
+				
+				testfile << "REFERENCE " << x << " " << y << "\n";
+				*/
+			}
+		}
+		if(extraRef == 1)
+		{
+			testfile << "REFERENCE " << numProc << " " << 1 << "\n";			
+		}
+		//write TERMINATE lines to file
+		for(int i = 1; i < numProc + 1; i++){
+			testfile << "TERMINATE " << i << "\n";
+
+		}
+	}
+
+
+	if(pattern == 1)//random access case
+	{
+		//write START lines to file
+		for(int i = 1; i < numProc + 1; i++)
+		{
+			testfile << "START " << i << " " << addrSize << "\n";
+
+		}
+		//write REFERENCE lines to file
+		for(int i = 0; i < numRef; i++){
+			
+			x = rand() % numProc + 1;
+			y = rand() % addrSize +  1;
+			testfile << "REFERENCE " << x << " " << y << "\n";
+
+		}
+		//write TERMINATE lines to file
+		for(int i = 1; i < numProc + 1; i++){
+
+			testfile << "TERMINATE " << i << "\n";
+
+		}
+	}
 	
 	testfile.close();
 	return 0;
